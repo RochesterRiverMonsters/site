0bc51efa49787237ce665028bccec0ace09242f4
Steve Buck <steve@rochesterrivermonsters.com>
2018-01-16 23:59:12 -0500

    added links for store and signup

--- src/SiteHeader.js
+++ src/SiteHeader.js
@@ -5,9 +5,15 @@ import './css/SiteHeader.css';
 export class SiteHeader extends React.Component {
   render() {
     return(
-          <div className="rrm-header">
-            <img className="brand" alt="Rochester River Monsters" src={logo}/>
-          </div>
+      <React.Fragment>
+        <div className="rrm-header">
+          <img className="brand" alt="Rochester River Monsters" src={logo}/>
+        </div>
+        <div className="rrm-menu-bar">
+          <div className="rrm-menu-items"><a href="https://www.facebook.com/events/920162731465101/">Sign Up</a><a href="https://www.warrior.com/on/demandware.store/Sites-warrior_us-Site/default/Team-MemberOrder?orderId=52203">Store</a><a href="https://www.warrior.com/on/demandware.store/Sites-warrior_us-Site/default/Team-MemberOrder?orderId=52205">Equipment</a></div>
+          <div className="rrm-menu-desc">code: ROCH2018</div>
+        </div>
+      </React.Fragment>
     )
   }
 }
