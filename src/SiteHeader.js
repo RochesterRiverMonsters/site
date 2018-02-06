45c5e1eba3a7b65ac937fbef9122ca8b4116c591
stevebuck <steve@softwarily.org>
2018-02-05 23:21:16 -0500

    Added to menu
    Added pricing information link to the menu.

--- src/SiteHeader.js
+++ src/SiteHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from './images/rrmbanner.png';
 import './css/SiteHeader.css';
 
@@ -10,7 +11,7 @@ export class SiteHeader extends React.Component {
           <img className="brand" alt="Rochester River Monsters" src={logo}/>
         </div>
         <div className="rrm-menu-bar">
-          <div className="rrm-menu-items"><a target="_blank" href="https://www.eventbrite.com/e/namla-rochester-river-monsters-youth-tryouts-tickets-41950976500">Sign Up</a><a target="_blank" href="https://www.warrior.com/on/demandware.store/Sites-warrior_us-Site/default/Team-MemberOrder?orderId=52203">Store</a><a target="_blank" href="https://www.warrior.com/on/demandware.store/Sites-warrior_us-Site/default/Team-MemberOrder?orderId=52205">Equipment</a></div>
+          <div className="rrm-menu-items"><a target="_blank" href="https://www.eventbrite.com/e/namla-rochester-river-monsters-youth-tryouts-tickets-41950976500">Sign Up</a><a target="_blank" href="https://www.warrior.com/on/demandware.store/Sites-warrior_us-Site/default/Team-MemberOrder?orderId=52203">Store</a><a target="_blank" href="https://www.warrior.com/on/demandware.store/Sites-warrior_us-Site/default/Team-MemberOrder?orderId=52205">Equipment</a><a href="/prices">Pricing</a></div>
           <div className="rrm-menu-desc">code: ROCH2018</div>
         </div>
       </React.Fragment>
