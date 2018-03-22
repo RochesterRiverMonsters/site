f42799cea6b871bb0a5e33fb7c33590de5df874d
Jackson Taber <jr_taber@yahoo.com>
2018-03-21 22:25:29 -0400

    RRMContentBox now has an id

--- src/RRMContentBox.js
+++ src/RRMContentBox.js
@@ -7,10 +7,11 @@ export class RRMContentBox extends React.Component{
   render(){
     const displayLogo = this.props.displayLogo;
     const displayButton = this.props.buttonLink;
+    const id = this.props.header.replace(/ +/g, "");;
     return(
       <div className="container rrm-content-box-slate">
           <div className="row">
-            <h1 className="text-left rrm-content-box-header">{this.props.header}</h1>
+            <h1 className="text-left rrm-content-box-header" id={id}>{this.props.header}</h1>
           </div>
             <div className="row">
           {displayLogo && <div className="col-sm-3"><Image className="rrm-content-box-headline-image" rounded={true} src={logo} alt="Rochester River Monsters"/></div>}
--- src/Youth.js
+++ src/Youth.js
@@ -8,26 +8,41 @@ export class Youth extends React.Component {
   render() {
     return(
       <div>
+
+
+      <RRMContentBox
+      header='Program'
+      body='Text goes here'
+      />
+
+
+      <br />
+
+
       <RRMContentBox
       displayLogo='true'
-      header='Program'
-      introText = 'This comes before the body!'
-      body='Text goes here'
-      bottomText='This is bottom text in bold'
-      buttonLink="https://google.com"
-      buttonText='Test'
-      />
-      <br />
-      <RRMContentBox
       header='NAMLA Tryouts'
-      body='Text goes here'
+      introText='The Rochester River Monsters are proud to offer an elite youth box lacrosse program in the Rochester area!'
+      body='Starting this year the Rochester River Monsters will offering PeeWee, Bantam and Midget teams competing in the North
+      American Minor Lacrosse Association (NAMLA). As part of this offering we will be playing games against top level talent from
+      near-by competitors such as the Onondaga Redhawks, Tonawanda Braves, Allegany Arrows, Newtown Golden Eagles and more, just as
+      our Mens team does. Tryouts for all three levels of this team will be Febrauary 23rd and practices will start in March.'
+      bottomText='Sign up to reserve your spot now!'
+      buttonLink='https://www.eventbrite.com/e/namla-rochester-river-monsters-youth-tryouts-tickets-41950976500'
+      buttonText='Sign Up'
       />
+
+
       <br />
+
+
       <RRMContentBox
       displayLogo='true'
       header='Q&A'
       body='Article'
       />
+
+
       </div>
     )
   }
