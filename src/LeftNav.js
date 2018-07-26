136cc0ec7198ced726859c8ebed3f16e3693b3f6
Jackson Taber <jr_taber@yahoo.com>
2018-07-25 21:09:05 -0400

    Updated LeftNav

--- src/LeftNav.js
+++ src/LeftNav.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import SideNav, {Nav, NavIcon, NavText} from 'react-sidenav';
+import { BrowserView, MobileView} from "react-device-detect";
 import './css/LeftNav.css';
 
 const BaseContainer = props =>
     <div className="left-nav-box"
         style={{
-            width: 175,
             ...props.style
         }}
     >
@@ -25,15 +25,19 @@ export class LeftNav extends React.Component{
 
   render() {
         return (
-          <BaseContainer style={{ background: '#000000', position: 'fixed', top: 0}}>
-            <SideNav highlightBgColor="#eee">
-            <h1 className="text-center left-nav-header">{this.props.navArray.title}</h1>
-              <Nav>
-                <NavText><a href="/">Home</a></NavText>
-              </Nav>
-              {this.makeNav(this.props.navArray)}
-            </SideNav>
-          </BaseContainer>
+          <BrowserView>
+            <BaseContainer style={{position: 'fixed', top: 0}}>
+              <SideNav highlightBgColor="#eee">
+                <h1 className="text-center left-nav-header">{this.props.navArray.title}</h1>
+                <div className="text-left">
+                  <Nav>
+                    <NavText><a href="/">Home</a></NavText>
+                  </Nav>
+                  {this.makeNav(this.props.navArray)}
+                </div>
+              </SideNav>
+            </BaseContainer>
+          </BrowserView>
         )
       }
     }
