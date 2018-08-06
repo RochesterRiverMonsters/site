eff8bfb79c9de06ca1cb5b3d9b48309b5a179e95
Jackson Taber <jr_taber@yahoo.com>
2018-08-05 21:10:50 -0400

    nav
    now has browser view and mobile view

--- src/LeftNav.js
+++ src/LeftNav.js
@@ -13,31 +13,48 @@ const BaseContainer = props =>
     </div>;
 
 
+
 export class LeftNav extends React.Component{
 
-  makeNav(navObj){
+  makeNav(navObj, view){
     var myNavArray = [];
     for(var i = 0; i < navObj.linkArray.length; i++){
-      myNavArray.push(<Nav><NavText><a href={navObj.linkArray[i]}>{navObj.textArray[i]}</a></NavText></Nav>)
+      if(view === "BrowserView"){
+        myNavArray.push(<Nav><NavText><a href={navObj.linkArray[i]}>{navObj.textArray[i]}</a></NavText></Nav>)
+      }
+      else if(view === "MobileView"){
+        myNavArray.push(<h3 className="text-center"><a href={navObj.linkArray[i]}>{navObj.textArray[i]}</a></h3>)
+      }
     }
     return (myNavArray);
   }
 
   render() {
         return (
-          <BrowserView>
-            <BaseContainer style={{position: 'fixed', top: 0}}>
-              <SideNav highlightBgColor="#eee">
-                <h1 className="text-center left-nav-header">{this.props.navArray.title}</h1>
-                <div className="text-left">
-                  <Nav>
-                    <NavText><a href="/">Home</a></NavText>
-                  </Nav>
-                  {this.makeNav(this.props.navArray)}
+          <div>
+            <BrowserView>
+              <BaseContainer style={{position: 'fixed', top: 0}}>
+                <SideNav highlightBgColor="#eee">
+                  <h1 className="text-center left-nav-header">{this.props.navArray.title}</h1>
+                  <div className="text-left">
+                    <Nav>
+                      <NavText><a href="/">Home</a></NavText>
+                    </Nav>
+                    {this.makeNav(this.props.navArray, "BrowserView")}
+                  </div>
+                </SideNav>
+              </BaseContainer>
+            </BrowserView>
+            <MobileView>
+              <div className="container left-nav-slate">
+                  <div className="row">
+                    <h1 className="text-center left-nav-header">{this.props.navArray.title}</h1>
+                  </div>
+                  {this.makeNav(this.props.navArray, "MobileView")}
                 </div>
-              </SideNav>
-            </BaseContainer>
-          </BrowserView>
+                <br />
+            </MobileView>
+          </div>
         )
       }
     }
